5746367693cd3d8c5e3faeeccdce3cc505e93e5f
DennisHable <dennishable@seznam.cz>
2026-08-26 23:38:42 +0200

    Fix: Clean root directory and keep only nas_backend

--- src/main/java/cz/dhable/projects/nas/model/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package cz.dhable.projects.nas.model.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/resources/db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE nas ENCODING 'UTF8'; -- vytvoření db; znaková sada
-GRANT ALL PRIVILEGES ON DATABASE nas TO admin1; -- udělení všech oprávnění; všechny tabulky v db nas
-GRANT ALL ON SCHEMA public TO admin1; -- uživ. musí mít oprávnění i do public, jinak v nový verzích Postgre nemůže vytvářet (Hibernate) tabulky
